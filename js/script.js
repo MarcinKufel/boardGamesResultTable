fe39a4187a2c7e1f27a69938ac01d8f157e6e732
Marcin Kufel <marcinkufel84@gmail.com>
2021-03-06 17:01:44 +0100

    add second player to result table with interaction

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 
 let result1 = document.querySelector(".result1");
+let result2 = document.querySelector(".result2");
 let allResults = document.querySelector(".allResults");
 let wynik = document.querySelector(".wynik");
 
@@ -17,9 +18,18 @@ allResults.addEventListener("click", (event) => {
     let v7 = +document.querySelector(".v7").value;
     let v8 = +document.querySelector(".v8").value;
 
+    let b2 = +document.querySelector(".b2").value;
+    let b3 = +document.querySelector(".b3").value;
+    let b4 = +document.querySelector(".b4").value;
+    let b5 = +document.querySelector(".b5").value;
+    let b6 = +document.querySelector(".b6").value;
+    let b7 = +document.querySelector(".b7").value;
+    let b8 = +document.querySelector(".b8").value;
+
   //  console.log(v2);
 
     result1.innerHTML = v2 + v3 + v4 + v5 + v6 + v7 + v8;
+    result2.innerHTML = b2 + b3 + b4 + b5 + b6 + b7 + b8;
 
   //  wynik.innerHTML = 'v2 + v3 + v4 + v5 + v6 + v7 + v8';
 });
